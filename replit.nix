c5a4ebbcb3b0e976baefef17ee4440edb7655db4
Michel Metran <michelmetran@gmail.com>
2023-03-26 14:12:11 -0300

    l

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.python38Full
+    pkgs.python38Full;
+    pkgs.replitPackages.prybar-python310
   ];
   env = {
     PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
