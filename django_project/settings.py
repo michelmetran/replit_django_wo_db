6495462ab922dd16037b6bccc6cb15167e09ac48
Michel Metran <michelmetran@gmail.com>
2023-03-28 01:29:53 -0300

    g

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,21 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-3!iy2yuwx2r5sauo$u##(=!!j6)#65olga4ci-@75eb4%%9uz%'
 
 
-if SECRET_KEY is None:
-    print(
-        "Please setup a SECRET_KEY in the Secrets (Environment variables) tab. See README.md for more."
-    )
-    # exit(1)
-
-    try:
-        config = dotenv_values(find_dotenv(usecwd=True))
-        SECRET_KEY = config['SECRET_KEY']
-        print('pegou do dd')
-    except:
-        pass
+# if SECRET_KEY is None:
+#     print(
+#         "Please setup a SECRET_KEY in the Secrets (Environment variables) tab. See README.md for more."
+#     )
+#     # exit(1)
+#
+#     try:
+#         config = dotenv_values(find_dotenv(usecwd=True))
+#         SECRET_KEY = config['SECRET_KEY']
+#         print('pegou do dd')
+#     except:
+#         pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
